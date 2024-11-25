5a8b34f4d33574c9e9b3437bde5b1f2ad2c627cf
lakshya-234 <lakshaychoudhary91@gmail.com>
2024-11-25 14:26:34 +0530

    Initial commit

---
+++ edusity/src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>© 2024 Edusity. All rights reserved.</p>
+      <ul>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
